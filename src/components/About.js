add868ab8d0a42193b3c98210ebea3a82ea2571e
Radiyah <rsabur58@gmail.com>
2021-04-26 16:52:00 -0400

    commit tree 6e84eab9fe38340bc550af8c421bcb1980ebe3a7

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  // if (!props.warn) {
+  //   return null;
+  // }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length >1 ?<p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <>
+        <h3>Links</h3>
+        <a href={props.github}>{props.github}</a><br />
+        <a href={props.linkedin}>{props.linkedin}</a>
+      </>
     </div>
   );
 }
+
 
 export default About;
